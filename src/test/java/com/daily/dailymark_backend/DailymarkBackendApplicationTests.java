621da5922f54baa1453090b1953e1184c59fc7c8
LeeYejin <dpwlszang@gmail.com>
2023-09-30 16:00:23 +0900

    feat: backend 프로젝트 기본환경 구성

---
+++ src/test/java/com/daily/dailymark_backend/DailymarkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daily.dailymark_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailymarkBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
